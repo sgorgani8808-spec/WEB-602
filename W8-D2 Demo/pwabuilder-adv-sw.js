db320d31bd4e617ecb75464959f5dcdda275332c
sgorgani8808-spec <s.gorgani.8808@westcliff.edu>
2026-03-01 19:12:36 -0800

    Project 2

---
+++ W8-D2 Demo/pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"6ee6081eeadb296a7644f6a99c610d64","url":"css/style.css"},{"revision":"f3d8bde849845984388913aa7dd13ea1","url":"index.html"},{"revision":"5b73fbd96cf336f2bcd8119a719e95e4","url":"js/main.js"},{"revision":"1d92cf0005d69c59ba8718a0d03682c0","url":"sw.js"}]);
